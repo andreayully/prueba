3ecc41b0bac822ed8a714c05defbda91c0421d4d
yully <yully.guzman@correounivalle.edu.co>
2016-12-06 21:12:51 -0500

    nueva forma de coneccion, error en la consulta

--- pandas_postgresql.py
+++ pandas_postgresql.py
@@ -1,6 +1,18 @@
-import psycopg2 as pg
+import psycopg2
 import pandas.io.sql as psql
+import numpy as np
 
-coneccion = pg.connect("dbname=pandas_db user=yully")
+# coneccion = pg.connect("dbname=pandas_db user=yully")
+#
+# dataframe = psql.frame_query("SELECT id_est, nota FROM est_nota", coneccion)
+# print(dataframe)
 
-dataframe = psql.frame_query("SELECT id_est, nota FROM est_nota", coneccion)
+con = psycopg2.connect(dbname='pandas_db', host='localhost', user='yully', password='yully', port='5432')
+cur = con.cursor()
+cur.execute("SELECT * FROM 'est_nota';")
+cur.fetchall()
+cur.close()
+con.close()
+
+data = np.array(cur.fetchall())
+print(data)
